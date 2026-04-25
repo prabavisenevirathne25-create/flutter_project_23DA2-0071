90e3845218032596c3dc38a371ccd59e6f640c81
Prabavi Senevirathne <prabavisenevirathne25@gmail.com>
2026-04-25 19:07:58 +0530

    Update custom_textfield.dart

--- lib/view/widgets/custom_textfield.dart
+++ lib/view/widgets/custom_textfield.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_project/utils/app_textstyles.dart';
+import '/utils/app_textstyles.dart';
 
 class CustomTextfield extends StatefulWidget {
   final String label;
