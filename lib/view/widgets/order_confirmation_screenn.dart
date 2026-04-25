cc023d1bec5b8efa778f79241db5a7d55c1e00ef
Prabavi Senevirathne <prabavisenevirathne25@gmail.com>
2026-04-25 19:09:31 +0530

    Update order_confirmation_screenn.dart

--- lib/view/widgets/order_confirmation_screenn.dart
+++ lib/view/widgets/order_confirmation_screenn.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_project/view/widgets/main_screen.dart';
+import '/view/widgets/main_screen.dart';
 import 'package:get/get.dart';
 
 class OrderConfirmationScreen extends StatelessWidget {
